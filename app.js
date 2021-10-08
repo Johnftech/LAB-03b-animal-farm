a5cf4cb8904550fcffc23b255a1c8faa98dafbcf
Johnftech <johnf.la.tech@gmail.com>
2021-10-08 10:27:07 -0700

    upload images, reference assets

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 // import functions
 
 // reference needed DOM elements
+//wolf 
+const wolf = document.getElemenyById('wolf-image');
+const wolfSound = document.getElementById('wolf-sound');
+//
+const condor = document.getElementById('condor-image');
+const condorSound = document.getElementById('condor-sound');
+// panther
+const panther = document.getElementById('panther-image');
+const pantherSound = document.getElementById('panther-sound');
 
 
 // set event listeners 
     // get user input(s)
     // do any needed work with the value(s)
     // update DOM to reflect new value(s)
+
